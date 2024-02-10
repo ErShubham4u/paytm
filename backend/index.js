78e404ef2e9875a83c6798bec64528e6a9101d51
ErShubham4u <connect2ershubham@gmail.com>
2024-02-10 16:53:21 +0530

    Added user signup functionality and authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,3 @@ app.use(express.json());
 
 app.use("/api/v1", mainRouter);
 app.listen(3000);
-
-// /api/v1/user/signup
-// /api/v1/user/signin
-// /api/v1/user/changePassword ....
-
-// /api/v1/account/transferMoney
-// /api/v1/account/balance
